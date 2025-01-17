1787ee2c551450b75a418bb9f039744414055408
canediandrestroyer <charmelion.criss@gmail.com>
2025-01-17 16:54:57 -0600

    add:Java templeate

---
+++ src/main/java/com/mycompany/wikianimales/WikiAnimales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wikianimales;
+
+/**
+ *
+ * @author charm
+ */
+public class WikiAnimales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
